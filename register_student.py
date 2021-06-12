4bc67398b14e4d87be887ba7468bfc438ce19012
LAPTOP-BBPVD7CJ\newyo <newyoung124@naver.com>
2021-06-12 12:24:56 +0900

    connected window with student table

--- register_student.py
+++ register_student.py
@@ -6,17 +6,85 @@ def insert1():
     student_ID = e_id.get()
     student_name = e_name.get()
     age = e_age.get()
-    textbook = e_textbook.get()
 
     if(student_ID=="" or student_name=="" or age=="") :
         MessageBox.showinfo("Insert Status", "All fields are required")
     else :
         con = mysql.connect(host="localhost", user="root", password="sky1575!!", database="english_school")
         cursor = con.cursor()
-        cursor.execute("insert into student(student_ID, student_name, age, textbook) values('"+ student_ID +"', '"+ student_name + "', '"+ age +"', '"+ textbook +"')")
+        cursor.execute("insert into student(student_ID, student_name, age) values('"+ student_ID +"', '"+ student_name + "', '"+ age +"')")
         cursor.execute("commit")
+
+        e_id.delete(0, 'end')
+        e_name.delete(0, 'end')
+        e_age.delete(0, 'end')
+        show()
         MessageBox.showinfo("Insert Status", "Inserted Succesfully")
         con.close()
+
+def delete() :
+    if(e_id.get() == ""):
+        MessageBox.showinfo("Delete Status", "ID is compolsary for delete")
+    else :
+        con = mysql.connect(host="localhost", user="root", password="sky1575!!", database="english_school")
+        cursor = con.cursor()
+        cursor.execute("delete from student where student_ID ='"+ e_id.get() +"'")
+        cursor.execute("commit")
+
+        e_id.delete(0, 'end')
+        e_name.delete(0, 'end')
+        e_age.delete(0, 'end')
+        show()
+        MessageBox.showinfo("Delete Status", "Deleted Succesfully")
+        con.close()
+
+def update() :
+    student_ID = e_id.get()
+    student_name = e_name.get()
+    age = e_age.get()
+
+    if(student_ID=="" or student_name=="" or age=="") :
+        MessageBox.showinfo("Update Status", "All fields are required")
+    else :
+        con = mysql.connect(host="localhost", user="root", password="sky1575!!", database="english_school")
+        cursor = con.cursor()
+        cursor.execute("update student set student_name = '"+ student_name +"', age = '"+ age +"' where student_ID = '"+ student_ID +"'")
+        cursor.execute("commit")
+
+        e_id.delete(0, 'end')
+        e_name.delete(0, 'end')
+        e_age.delete(0, 'end')
+        show()
+        MessageBox.showinfo("Update Status", "Updated Succesfully")
+        con.close()
+
+
+def get() :
+    if(e_id.get() == ""):
+        MessageBox.showinfo("Fetch Status", "ID is compolsary for delete")
+    else :
+        con = mysql.connect(host="localhost", user="root", password="sky1575!!", database="english_school")
+        cursor = con.cursor()
+        cursor.execute("select * from student where student_ID ='"+ e_id.get() +"'")
+        rows = cursor.fetchall()
+
+        for row in rows:
+            e_name.insert(0, row[1])
+            e_age.insert(0, row[2])
+        con.close()
+
+def show() :
+    con = mysql.connect(host="localhost", user="root", password="sky1575!!", database="english_school")
+    cursor = con.cursor()
+    cursor.execute("select * from student")
+    rows = cursor.fetchall()
+    list.delete(0, list.size())
+
+    for row in rows:
+        insertData = str(row[0]) + '          ' + row[1]
+        list.insert(list.size()+1, insertData)
+
+    con.close()
 
 if __name__ == "__main__" :
     win1 = Tk()
@@ -47,5 +115,18 @@ if __name__ == "__main__" :
 
     insert = Button(win1, text="insert", font=('italic', 10), bg = 'white', command=insert1)
     insert.place(x = 20, y = 190)
+
+    delete = Button(win1, text="delete", font=("italic", 10), bg="white", command=delete)
+    delete.place(x = 70, y = 190)
+
+    update = Button(win1, text="update", font=("italic", 10), bg="white", command = update)
+    update.place(x = 122.5, y = 190)
+
+    get = Button(win1, text="get", font=("italic", 10), bg="white", command = get)
+    get.place(x = 181, y = 190)
+
+    list = Listbox(win1)
+    list.place(x=290, y = 30)
+    show()
 
     win1.mainloop()
