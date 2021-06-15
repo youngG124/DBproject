1121ccbf49a86e0b079b7bf532ffad68dd3a6cb4
LAPTOP-BBPVD7CJ\newyo <newyoung124@naver.com>
2021-06-15 18:27:20 +0900

    delete 버튼 구현

--- att_list.py
+++ att_list.py
@@ -3,14 +3,15 @@ import tkinter.messagebox as MessageBox
 import mysql.connector as mysql
 import tkinter.ttk as ttk
 
-def show() :
+def show_today() :
     con = mysql.connect(host="localhost", user="root", password="sky1575!!", database="english_school")
     cursor = con.cursor()
     cursor.execute("select s.student_name, a.a_time, t.teacher_name \
         from student s, attendance a, teacher t \
             where s.student_id = a.student_id \
                 and t.teacher_id = a.check_teacher \
-                    and date(a_time) = date(now())")
+                    and date(a_time) = date(now()) \
+                        order by a_time asc")
     rows = cursor.fetchall()
     list.delete(0, list.size())
     for row in rows :
@@ -28,7 +29,8 @@ def search() :
         from student s, attendance a, teacher t \
             where s.student_id = a.student_id \
                 and t.teacher_id = a.check_teacher \
-                    and s.student_name = '" + student_name + "'")
+                    and s.student_name = '" + student_name + "' \
+                        order by a_time asc")
     rows = cursor.fetchall()
     list.delete(0, list.size())
     for row in rows :
@@ -37,11 +39,37 @@ def search() :
 
     con.close()
 
+def delete() :
+    # 리스트에서 이름만 가져오기
+    a = str(list.get(ANCHOR))
+    delete_name = a[0] + a[1] + a[2]
+    print(delete_name)
+
+    # 리스트에서 가져온 학생 이름으로 아이디 가져오기
+    con = mysql.connect(host="localhost", user="root", \
+         password="sky1575!!", database="english_school")
+    cursor = con.cursor()
+    cursor.execute("select student_ID from student \
+        where student_name = '" + delete_name + "'")
+    delete_id = [item[0] for item in cursor.fetchall()]
+    delete_id2 = str(delete_id[0])
+
+    print(delete_id2)
+
+    # 가져온 아이디 사용해서 삭제
+    cursor.execute("delete from attendance \
+        where student_ID = '" + delete_id2 + "'\
+            and date(a_time) = date(now())")
+    cursor.execute("commit")
+    show_today()
+    MessageBox.showinfo("Delete Status", "Deleted Succesfully")
+    con.close()
+    
 
 if __name__ == "__main__" :
     attlist = Tk()
-    attlist.geometry("500x300")
-    attlist.title("attendance list")
+    attlist.geometry("315x300")
+    attlist.title("attendance list(today)")
 
     # 학생 이름 불러오기
     con = mysql.connect(host="localhost", user="root", \
@@ -51,14 +79,18 @@ if __name__ == "__main__" :
     students = [item[0] for item in cursor.fetchall()]
     # 불러온 이름 콤보박스에 넣기
     student_combo = ttk.Combobox(attlist, height = 5, values = students)
-    student_combo.pack(pady = 15)
+    student_combo.place(x = 30, y = 20)
     student_combo.set("학생 선택")
     # 검색 버튼
     search = Button(attlist, text="검색", font=('italic', 10), bg = 'white', command = search)
-    search.pack(pady = 1)
+    search.place(x = 200, y = 20)
+    # 삭제 버튼
+    delete = Button(attlist, text="삭제", font=('italic', 10), bg = 'white', command = delete)
+    delete.place(x = 242.5, y = 20)
+
 
     list = Listbox(attlist, width = 35)
-    list.pack(pady = 15)
+    list.place(x = 30, y = 90)
 
-    show()
+    show_today()
     attlist.mainloop()
